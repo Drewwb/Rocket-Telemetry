ccbe76e0889811653b9ab5b223753dcca3ee26bc
Drew Brown <drew_brown@frontier.com>
2026-05-08 13:49:22 -0700

    feat: dvelte frontedn connected to websocket stream

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+
+    let message = "Waiting for telemetry..";
+
+    onMount(() => {
+        const ws = new WebSocket('ws://localhost:3000/ws');
+        
+        ws.onmessage = (event) => {
+            message = event.data;
+        };
+    });
+</script>
+
+<main>
+  <h1>🚀 Rocket Telemetry</h1>
+  <p>{message}</p>
+</main>
